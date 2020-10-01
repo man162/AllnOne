468b59a25c2dc9f72208f0c1040fbd336cad44a3
Manpreet Singh <manpreet.singh@cineplex.com>
2020-09-30 23:29:40 -0400

    adding project alInOne

---
+++ alinone/alInOne.h
@@ -0,0 +1,18 @@
+//
+//  alInOne.h
+//  alInOne
+//
+//  Created by Manpreet Singh on 2020-09-30.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for alInOne.
+FOUNDATION_EXPORT double alInOneVersionNumber;
+
+//! Project version string for alInOne.
+FOUNDATION_EXPORT const unsigned char alInOneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <alInOne/PublicHeader.h>
+
+
